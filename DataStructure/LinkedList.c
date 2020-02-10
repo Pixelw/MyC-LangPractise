0e3abeb3222c2524209954f67212f5def3d18132
Pixel <sulycarl34@gmail.com>
2020-02-10 20:49:45 +0800

    F bug

--- DataStructure/LinkedList.c
+++ DataStructure/LinkedList.c
@@ -5,24 +5,26 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-typedef char Data;
-typedef struct Node {
-    Data data;
-    struct Node *next;
-} ListNode;
+#define FAILED -1
 
-typedef ListNode *LinkList;
+typedef char Data;
+typedef struct ListNode {
+    Data data;
+    struct ListNode *next;
+} Node;
+
+typedef Node *LinkList;
 
 /***
  * 生成新节点
  * @return 新节点
  */
-ListNode *makeNewNode() {
-    ListNode *pNode = (ListNode *) malloc(sizeof(ListNode));
+Node *makeNewNode() {
+    Node *pNode = (Node *) malloc(sizeof(Node));
     if (pNode) {
         return pNode;
     }
-    printf("节点申请失败");
+    printf("malloc failed\n");
     return NULL;
 }
 
@@ -30,11 +32,12 @@ ListNode *makeNewNode() {
  * 头插法建立单链表
  * @return head 单链表
  */
+ //TODO bug
 LinkList createListF() {
     Data ch;
-    ListNode *pNode;
+    Node *pNode;
     LinkList pHead = NULL;
-    printf("输入链表各个节点的顺序\n");
+    printf("输入链表各个节点的数据(char)\n");
     while ((ch = getchar()) && ch != '\n') {
         pNode = makeNewNode();
         if (pNode == NULL) {
@@ -53,13 +56,13 @@ LinkList createListF() {
  */
 LinkList createListE() {
     Data ch;
-    ListNode *pNode, *pEndNode;
+    Node *pNode, *pEndNode;
     LinkList linkHead = makeNewNode();
     if (linkHead == NULL) {
         return NULL;
     }
     pEndNode = linkHead;
-    printf("请输入链表各节点数据\n");
+    printf("输入链表各个节点的数据(char)\n");
     while ((ch = getchar()) && ch != '\n') {
         pNode = makeNewNode();
         if (pNode == NULL) {
@@ -77,12 +80,12 @@ LinkList createListE() {
 /***
  * 查找第N个节点
  * @param head 链表的头节点
- * @param n 第N个节点
+ * @param n
  * @return 找到的节点
  */
-ListNode *getNodeN(LinkList head, int n) {
+Node *getNodeN(LinkList head, int n) {
     int count = 1;
-    ListNode *pNode = head->next;
+    Node *pNode = head->next;
     while (pNode && count < n) {
         pNode = pNode->next;
         count++;
@@ -100,12 +103,23 @@ ListNode *getNodeN(LinkList head, int n) {
  * @param data 值
  * @return 节点
  */
-ListNode *locateNode(LinkList list, Data data) {
-    ListNode *pNode = list->next;
+Node *locateNode(LinkList list, Data data) {
+    Node *pNode = list->next;
     while (pNode && pNode->data != data) {
         pNode = pNode->next;
     }
     return pNode;
+}
+
+int locateNodePos(LinkList list, Data data){
+    Node *pNode = list->next;
+    int i = 0;
+    while (pNode && pNode->data != data) {
+        i ++;
+        pNode = pNode->next;
+
+    }
+    return i;
 }
 
 /***
@@ -114,40 +128,102 @@ ListNode *locateNode(LinkList list, Data data) {
  * @return 长度
  */
 int getListLength(LinkList list) {
-    ListNode *node = list->next;
+    Node *node = list->next;
     int length = 0;
     while (node) {
-        node = node->next;
         length++;
+        node = node->next;
     }
     return length;
 }
 
 /***
- * 插入新节点到n
+ * 插入新节点到 n
  * @param list 链表
  * @param data 插入的数据
- * @param n 位置
+ * @param n
  * @return 成功则返回插入的位置 @param n
  */
-int insertList(LinkList list, Data data, int n) {
-    ListNode *pNode, *newNode;
-    pNode = getNodeN(list, n - 1);
-    if (pNode == NULL) {
-        printf("the node %d not found", n - 1);
-        return -1;
+int insertNode(LinkList list, Data data, int n) {
+    Node *pPrevNode, *pNewNode;
+    pPrevNode = getNodeN(list, n - 1);
+    if (pPrevNode == NULL) {
+        printf("the previous node of node_%d not found\n", n);
+        return FAILED;
     }
-    newNode = makeNewNode();
-    if (newNode == NULL) {
-        return -1;
+    pNewNode = makeNewNode();
+    if (pNewNode == NULL) {
+        return FAILED;
     }
-    newNode->data = data;
-    newNode->next = pNode->next;
-    pNode->next = newNode;
+    pNewNode->data = data;
+    pNewNode->next = pPrevNode->next;
+    pPrevNode->next = pNewNode;
     return n;
 }
 
+/***
+ * 删除第 n 个节点
+ * @param list 链表
+ * @param n
+ * @return 成功则返回删除的位置 @param n
+ */
+int removeNodeN(LinkList list, int n){
+    Node *pTargetNode, *pPrevNode;
+    pPrevNode = getNodeN(list, n - 1);
+    if (pPrevNode == NULL) {
+        printf("the previous node of node_%d not found\n", n);
+        return FAILED;
+    }
+    pTargetNode = pPrevNode->next;
+    //跳过
+    pPrevNode->next = pTargetNode->next;
+    //释放
+    free(pTargetNode);
+    return n;
+}
+
+/***
+ * 销毁链表
+ * @param head 链表（头节点）
+ */
+void destroyList(LinkList head){
+     Node *pA, *pB;
+     pA = (Node *)head;
+    while(pA){
+        pB = pA->next;
+        free(pA);
+        pA = pB;
+    }
+    head = NULL;
+}
+
+void printList(LinkList list){
+    Node *node = list ->next;
+    printf("LinkedList:");
+    while (node){
+        printf(" %c",node->data);
+        node = node->next;
+    }
+    printf(";\n");
+
+}
 
 int main() {
+    LinkList l = createListE();
+    printList(l);
+    printf("%d\n",getListLength(l));
+//    Node *node = getNodeN(linkList, 3);
+//    printf("3 is %c",node->data);
+    insertNode(l, 's', 2);
+    printList(l);
+    Node *node;
+    node = locateNode(l, '-');
+    if (node){
+        printf("%c",node->data);
+    }
+    removeNodeN(l, 3);
+    printList(l);
+//    destroyList(l);
+//    printList(l);
 
 }
