30edd3aaa60adaec9255efec9437f77d5f9b7ae1
Pixel <sulycarl34@gmail.com>
2020-02-13 18:59:57 +0800

    move to Util

--- Util/printincolor.h
+++ Util/printincolor.h
@@ -5,12 +5,19 @@
 #ifndef DATASTRUCTURE_PRINTINCOLOR_H
 
 /**
+ * Windows API
  * color used by printf_in_color_win
  */
 enum console_color {
+    Black = 0,
+    White = 7,
+    Gray = 8,
     Red = 4,
     Green = 2,
-    Blue = 1
+    Blue = 1,
+    Yellow = 6,
+    Purple = 5,
+    Aqua = 3
 };
 
 /**
@@ -20,6 +27,10 @@ enum console_color {
  * @param color
  */
 void printf_in_color_win(enum console_color color, char *str);
+
+/**
+ * Print string in red
+ */
 
 void printf_in_red_ansi(char *str);
 
